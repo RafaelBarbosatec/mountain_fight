ca09f39a037fbbebb46c368f7ce8a1e1cd85417b
Rafael <rafaelbarbosatec@gmail.com>
2020-04-25 12:14:09 -0300

    atualiza velocidade confomr tileSize

--- lib/player/game_player.dart
+++ lib/player/game_player.dart
@@ -19,7 +19,7 @@ class GamePlayer extends Player {
             height: tileSize * 1.5,
             initPosition: initPosition,
             life: 200,
-            speed: 2.5,
+            speed: tileSize / 13,
             collision: Collision(height: 16, width: 16));
 
   void showEmote(Animation emoteAnimation) {
